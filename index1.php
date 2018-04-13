92b9eb4faab0a0d835f9da1b83352fbecd4ed884
AlexeyIpatov <lexa_pranik@mail.ru>
2018-04-13 03:59:31 +0300

    2

--- index1.php
+++ index1.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
  /* Здесь проверяется существование переменных */
   if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
  if (isset($_POST['name'])) {$name = $_POST['name'];}
@@ -12,7 +12,7 @@
 
 /* А эта функция как раз занимается отправкой письма на указанный вами email */
 $sub='Заказ с сайта'; //сабж
-$email='<yourdomen.ru>'; // от кого
+$email='<noreply@github.com>'; // от кого
  $send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
 
 ini_set('short_open_tag', 'On');
